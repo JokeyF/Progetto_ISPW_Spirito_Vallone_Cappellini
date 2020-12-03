2e6087e6aa8d4586869ab1f5a188348982b042d8
Jokey <48331233+JokeyF@users.noreply.github.com>
2020-12-03 19:52:25 +0100

    aggiunto il progetto helloworld fatto con eclipse, avente all'interno il file build.xml eseguibile con ant. Ho anche spostato il file travis all'interno della cartella del progetto HelloWorld

---
+++ HelloWorldProject/src/pacchetto/inventato/HelloWorld.java
@@ -0,0 +1,7 @@
+package pacchetto.inventato;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!");
+	}
+}
