2d693177309584a857379cfb6069c94dc3857e5f
Jokey <48331233+JokeyF@users.noreply.github.com>
2020-12-04 12:19:57 +0100

    annullato il precedente commit di rottura

--- HelloWorldProject/src/pacchetto/inventato/HelloWorld.java
+++ HelloWorldProject/src/pacchetto/inventato/HelloWorld.java
@@ -2,6 +2,6 @@ package pacchetto.inventato;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!")
+		System.out.println("Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!Ciao Mondo!!!");
 	}
 }
